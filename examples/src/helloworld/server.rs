4b5244f4ddb63d94688a8b005a55fa9eb307ba5d
Ian Purton <ian.purton@gmail.com>
2021-03-23 19:39:45 +0000

    Moved code into generator.

--- examples/src/helloworld/server.rs
+++ examples/src/helloworld/server.rs
@@ -1,8 +1,5 @@
-use actix_web::{Error, get, middleware, web, App, HttpRequest, web::Bytes, HttpResponse, HttpServer, Responder, Result};
+use actix_web::{middleware, web, App, HttpServer, Result};
 use hello_world::{HelloReply, HelloRequest, greeter_server};
-use crate::hello_world::greeter_server::Greeter;
-use base64;
-use prost::Message;
 use async_trait::async_trait;
 
 pub mod hello_world {
@@ -20,18 +17,6 @@ impl greeter_server::Greeter for GreeterImpl {
     }
 }
 
-async fn greeter_say_hello(body: Bytes, greeter: web::Data<Box<dyn greeter_server::Greeter>>) -> impl Responder {
-    
-    let buffer = base64::decode(body).unwrap();
-    let s = HelloRequest::decode(buffer.as_ref()).unwrap();
-
-    let reply = greeter.into_inner().say_hello(s).await.unwrap();
-
-    let mut proto_buffer: Vec<u8> = Vec::new();
-    reply.encode(&mut proto_buffer).unwrap();
-    base64::encode(proto_buffer)
-}
-
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
@@ -39,7 +24,7 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
             .data::<Box<dyn greeter_server::Greeter>>(Box::new(GreeterImpl {}))
-            .route("/helloworld/Greeter/SayHello", web::post().to(greeter_say_hello))
+            .route("/helloworld/Greeter/SayHello", web::post().to(greeter_server::say_hello))
             .wrap(middleware::Logger::default())
     })
     .bind("0.0.0.0:8080")?
