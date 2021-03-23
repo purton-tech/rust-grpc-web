ba3d72539a4501153ff11912131c87aebba62af9
Ian Purton <ian.purton@gmail.com>
2021-03-23 10:44:29 +0000

    Initial commit.

--- examples/src/helloworld/client.rs
+++ examples/src/helloworld/client.rs
@@ -4,7 +4,7 @@ use std::collections::HashMap;
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let client = reqwest::Client::new();
     let resp = client.post("http://localhost:8080/helloworld/Greeter/SayHello")
-        .body("the exact body that is sent")
+        .body(b"the exact body that is sent")
         .send()
         .await?;
     println!("{:#?}", resp.text().await?);
