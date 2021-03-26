b078efb517148293df2a4e03b27326989799c6ef
Ian Purton <ian.purton@gmail.com>
2021-03-26 08:29:14 +0000

    [ci skip] Example working.

--- examples/js-client/src/index.js
+++ examples/js-client/src/index.js
@@ -1,7 +1,7 @@
-const {HelloRequest, HelloReply} = require('../helloworld_pb.js');
-const {GreeterClient} = require('../helloworld_grpc_web_pb.js');
+const {HelloRequest, HelloReply} = require('./helloworld_pb.js');
+const {GreeterClient} = require('./helloworld_grpc_web_pb.js');
 
-var client = new GreeterClient('localhost:8080');
+var client = new GreeterClient('http://localhost:8080');
 
 var request = new HelloRequest();
 request.setName('World');
