eb0bb8517344a4e94f2a21211ca051e95fa7b44e
Ian Purton <ian.purton@gmail.com>
2021-04-17 14:06:00 +0000

    Add in web sockets.

--- examples/seed-wasm-client/src/lib.rs
+++ examples/seed-wasm-client/src/lib.rs
@@ -8,39 +8,117 @@ pub mod hello_world {
 }
 
 use seed::{prelude::*, *};
+use std::rc::Rc;
 
 // ------ ------
 //     Init
 // ------ ------
 
 // `init` describes what should happen when your app started.
-fn init(_: Url, _: &mut impl Orders<Msg>) -> Model {
-    Model::default()
+fn init(_: Url, orders: &mut impl Orders<Msg>) -> Model {
+    Model {
+        web_socket: create_websocket(orders),
+        web_socket_reconnector: None,
+    }
 }
 
 // ------ ------
 //     Model
 // ------ ------
 
-// `Model` describes our app state.
-type Model = i32;
+pub struct Model {
+    web_socket: WebSocket,
+    web_socket_reconnector: Option<StreamHandle>
+}
 
 // ------ ------
 //    Update
 // ------ ------
 
-// (Remove the line below once any of your `Msg` variants doesn't implement `Copy`.)
-//#[derive(Copy, Clone)]
-// `Msg` describes the different events you can modify state with.
-enum Msg {
-    Increment
+
+pub enum Msg {
+    WebSocketOpened,
+    CloseWebSocket,
+    WebSocketClosed(CloseEvent),
+    WebSocketFailed,
+    ReconnectWebSocket(usize),
+    InputTextChanged(String),
+    InputBinaryChanged(String),
 }
 
-// `update` describes how to handle each `Msg`.
-fn update(msg: Msg, model: &mut Model, orders: &mut impl Orders<Msg>) {
+fn update(msg: Msg, mut model: &mut Model, orders: &mut impl Orders<Msg>) {
     match msg {
-        Msg::Increment => {
-        },
+        Msg::WebSocketOpened => {
+            model.web_socket_reconnector = None;
+            log!("WebSocket connection is open now");
+        }
+        Msg::CloseWebSocket => {
+            model.web_socket_reconnector = None;
+            model
+                .web_socket
+                .close(None, Some("user clicked Close button"))
+                .unwrap();
+        }
+        Msg::WebSocketClosed(close_event) => {
+            log!("==================");
+            log!("WebSocket connection was closed:");
+            log!("Clean:", close_event.was_clean());
+            log!("Code:", close_event.code());
+            log!("Reason:", close_event.reason());
+            log!("==================");
+
+            // Chrome doesn't invoke `on_error` when the connection is lost.
+            if !close_event.was_clean() && model.web_socket_reconnector.is_none() {
+                model.web_socket_reconnector = Some(
+                    orders.stream_with_handle(streams::backoff(None, Msg::ReconnectWebSocket)),
+                );
+            }
+        }
+        Msg::WebSocketFailed => {
+            log!("WebSocket failed");
+            if model.web_socket_reconnector.is_none() {
+                model.web_socket_reconnector = Some(
+                    orders.stream_with_handle(streams::backoff(None, Msg::ReconnectWebSocket)),
+                );
+            }
+        }
+        Msg::ReconnectWebSocket(retries) => {
+            log!("Reconnect attempt:", retries);
+            model.web_socket = create_websocket(orders);
+        }
+        Msg::InputTextChanged(text) => {
+        }
+        Msg::InputBinaryChanged(text) => {
+        }
+    }
+}
+
+fn create_websocket(orders: &impl Orders<Msg>) -> WebSocket {
+    let msg_sender = orders.msg_sender();
+
+    WebSocket::builder("ws://localhost:8080", orders)
+        .on_open(|| Msg::WebSocketOpened)
+        .on_message(move |msg| decode_message(msg, msg_sender))
+        .on_close(Msg::WebSocketClosed)
+        .on_error(|| Msg::WebSocketFailed)
+        .build_and_open()
+        .unwrap()
+}
+
+fn decode_message(message: WebSocketMessage, msg_sender: Rc<dyn Fn(Option<Msg>)>) {
+    if message.contains_text() {
+
+        //msg_sender(Some(Msg::TextMessageReceived(msg)));
+    } else {
+        spawn_local(async move {
+            let bytes = message
+                .bytes()
+                .await
+                .expect("WebsocketError on binary data");
+
+            //let msg: shared::ServerMessage = rmp_serde::from_slice(&bytes).unwrap();
+            //msg_sender(Some(Msg::BinaryMessageReceived(msg)));
+        });
     }
 }
 
